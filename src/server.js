401bafaf9fc0091d783f067223c1a8dc06ccab1c
Le Hoang Trong <langtupro0456@gmail.com>
2023-12-20 15:48:48 +0700

    khởi tạo server trello

---
+++ src/server.js
@@ -0,0 +1,16 @@
+// Viết các cấu hình của server vào trong đây
+
+const express = require('express');
+
+const app = express();
+
+const PORT = 8017;
+const HOSTNAME = 'localhost';
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOSTNAME, () => {
+  console.log(`Hello Lê Hoàng Trong, I'm running server at http://${HOSTNAME}:${PORT}/ `);
+});
