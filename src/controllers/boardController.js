97b00e1aa9db7535784e8771925b8b13af8e9ede
Le Hoang Trong <langtupro0456@gmail.com>
2023-12-24 19:09:39 +0700

    code logic ở tầng service

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -11,8 +11,12 @@ import { StatusCodes } from 'http-status-codes'
 const createNew = async (req, res, next) => {
   try {
     // console.log('Req Body', req.body)
+
+    // Điều hướng dữ liệu sang tầng service
+    const createdBoard = await boardService.createNew(req.body)
+
     // throw new ApiError(StatusCodes.BAD_GATEWAY, 'HoangTrongDev test Error Server!!!')
-    res.status(StatusCodes.CREATED).json({ message: 'POST from Validation: API get new boards' })
+    res.status(StatusCodes.CREATED).json(createdBoard)
   } catch (error) {
     next(error)
     // res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
