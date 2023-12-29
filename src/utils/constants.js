6b07b64362e3d70680f47cc79415e6be3b5fdd3b
Le Hoang Trong <langtupro0456@gmail.com>
2023-12-30 00:46:12 +0700

    thêm domain phía FE vào whitelist

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 // Những DOMAIN được phép truy cập tới tài nguyên của SERVER
 export const WHITELIST_DOMAINS = [
+  'https://trello-clone-myproject.vercel.app'
   // 'http://localhost:3000' // Không cần localhost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
   // Ví dự sau này sẽ deploy lên domain chính thức ,...vvv
 ]
